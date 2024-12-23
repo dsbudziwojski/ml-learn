4f944ce95da90feb9f039ba159ef19d01d1aaec0
David Budziwojski <dsbudziwojski@gmail.com>
2024-12-22 20:46:38 -0800

    LinearRegression: implemented lms_gradient_descent and modified fit

--- linear_regression.py
+++ linear_regression.py
@@ -5,6 +5,8 @@ class linear_regression(Model):
         self.X = None
         self.Y = None
         self.theta = None
+        self.alpha = 0.01
+        self.epochs = 50
 
     def hypothesis(self, j):
         return np.dot(self.theta.T,self.X[:,j])
@@ -13,9 +15,15 @@ class linear_regression(Model):
         self.X[1:np.shape(X)[0],1:np.shape(X)[1]] = X
         self.Y = np.array(Y)
         self.theta = np.zeros(X.shape[1]) # initial parameters are set to 0
-        self.lms_gradient_descent()
+        for i in range(self.epochs):
+            self.lms_gradient_descent()
+
     def lms_gradient_descent(self):
-        # ToDo
+        for j in range(self.X.shape[0]):
+            sum_delta = 0
+            for i in range(self.X.shape[1]):
+                sum_delta += (self.Y[i,0] - self.hypothesis(i)) * self.X[j,i]
+            self.theta[j] += self.alpha * sum_delta
 
 
 
